2c81160d8f818b261108572a854583fffec90139
snowman-1030 <sinju1030@gmail.com>
2025-02-20 14:43:04 +0900

    Initial commit

---
+++ src/lib/config.ts
@@ -0,0 +1,6 @@
+export const config = {
+    CLIENT_ID: "1319804369327358002", // Discord Application ID
+    COMPANY_NAME: "Solciety", // Company or Developer Name
+    PRODUCT_NAME: "Solciety", // Game Name
+    PRODUCT_VERSION: "0.1.0", // Game Version
+};
